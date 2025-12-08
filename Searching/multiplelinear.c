89ffeeecf14a09460504a56db121b46635751d4d
alrifatsabbir <alrifatsabbir@gmail.com>
2025-12-08 16:06:23 +0600

    searching-v-0.0.2

---
+++ Searching/multiplelinear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, target, found = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        printf("Enter the %d element: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the element you wanted: ");
+    scanf("%d", &target);
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            printf("Found at %d\n", i+1);
+            found = 1;
+        }
+    }
+    if(!found){
+        printf("Not found\n");
+    }
+    return 0;
+}
